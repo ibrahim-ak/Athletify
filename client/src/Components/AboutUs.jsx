b0129eea85a10b5eff3ed4e238ca5b7415755cf1
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-08-22 22:28:07 +0300

    some touches in the main page ,, location and justify the text and image in about us

--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -2,16 +2,16 @@ import { Box, Typography, Container, Grid, Card, CardContent, CardMedia } from '
 
 function AboutUs() {
   return (
-    <Box sx={{ py: 8, backgroundColor: '#ffb65da1' , marginBottom:'30px'}}>
+    <Box sx={{ py: 8, backgroundColor: '#ffb65da1', marginBottom: '30px' }}>
       <Container maxWidth="lg">
         <Grid container spacing={6} alignItems="center">
           {/* Image Section */}
           <Grid item xs={12} md={6}>
             <CardMedia
               component="img"
-              image="/src/media/image copy 2.png" // Adjust the image path as needed
+              image="/src/media/aboutusimage.png" // Adjust the image path as needed
               alt="Academy Image"
-              sx={{ borderRadius: '12px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)' }}
+            // sx={{ borderRadius: '12px', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)' }}
             />
           </Grid>
 
@@ -19,15 +19,15 @@ function AboutUs() {
           <Grid item xs={12} md={6}>
             <CardContent>
               <Typography variant="h3" component="h2" gutterBottom sx={{ fontWeight: 'bold', color: '#1d4f67' }}>
-                About <span style={{ color: '#fb841a' }}>Athletify</span>
+                About <span style={{ color: '#fffce8' }}>Athletify</span>
               </Typography>
-              <Typography variant="body1" paragraph sx={{ color: '#4f5d75', lineHeight: 1.8 }}>
+              <Typography variant="body1" paragraph sx={{ color: '#4f5d75', lineHeight: 1.8, textAlign: 'justify' }}>
                 Athletify is a platform designed specifically for athletic academies, offering a comprehensive solution to manage
                 all aspects of academy administration. Our platform simplifies tasks such as student information management,
                 training schedules, announcements, and payments, making it easier for owners and sub-admins to keep everything
                 running smoothly.
               </Typography>
-              <Typography variant="body1" paragraph sx={{ color: '#4f5d75', lineHeight: 1.8 }}>
+              <Typography variant="body1" paragraph sx={{ color: '#4f5d75', lineHeight: 1.8, textAlign: 'justify' }}>
                 By streamlining these essential functions into a single, user-friendly interface, Athletify reduces the time and effort
                 required to manage an academy. Our goal is to provide a more organized, efficient, and responsive management
                 experience, so you can focus on what matters most: training and developing your athletes.
