17debac79c36cc206f393c4fd0260eaf1447fc43
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-08-25 22:02:36 +0300

    finished cards hover

--- client/src/Components/group/AllGroupsCards.jsx
+++ client/src/Components/group/AllGroupsCards.jsx
@@ -5,16 +5,13 @@ import { Link } from 'react-router-dom';
 
 const AllGroupsCards = () => {
   const [groups, setGroups] = useState([]);
-  const [academy, setAcademy] = useState("");
 
   useEffect(() => {
-      const academyId = localStorage.getItem('id');
-      // setAcademy(academyId);
-      
+    const academyId = localStorage.getItem('id');
+
     const fetchGroups = async () => {
       try {
         const response = await axios.get(`http://localhost:8000/api/group/academy/${academyId}`);
-        // console.log(response.data.groups)
         setGroups(response.data.groups);
       } catch (error) {
         console.error('Error fetching groups:', error);
@@ -22,22 +19,31 @@ const AllGroupsCards = () => {
     };
 
     fetchGroups();
-  }, [groups]);
+  }, []);
 
   return (
     <div>
       <Grid container spacing={3}>
         {groups.map((group) => (
-          
           <Grid item xs={12} sm={6} md={4} key={group._id}>
             <Link to={`/academy/academy-chat/${group._id}`} style={{ textDecoration: 'none' }}>
-            <Card>
-              <CardContent>
-                <Typography variant="h6" component="div" align='center'>
-                  {group.Name}
-                </Typography>
-              </CardContent>
-            </Card>
+              <Card 
+                sx={{
+                  transition: 'transform 0.3s, box-shadow 0.3s',
+                  '&:hover': {
+                    transform: 'scale(1.05)',
+                    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+                    backgroundColor: '#001f3f', // Dark navy color on hover
+                    color: 'white', // Text color on hover
+                  }
+                }}
+              >
+                <CardContent>
+                  <Typography variant="h6" component="div" align="center">
+                    {group.Name}
+                  </Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
         ))}
