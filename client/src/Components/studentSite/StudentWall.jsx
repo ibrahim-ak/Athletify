f44d20dd97046a23a6493387352843c4d41e7529
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-08-25 16:26:59 +0300

    try1

--- client/src/Components/studentSite/StudentWall.jsx
+++ client/src/Components/studentSite/StudentWall.jsx
@@ -5,7 +5,7 @@ import Announcements from '../Announcements';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import StudentSiteAnnouncements from './StudentSiteAnnouncements'
-import {Grid, Typography, Paper, Container, Divider } from '@mui/material';
+import {Box, Grid, Typography, Paper, Container, Divider } from '@mui/material';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
